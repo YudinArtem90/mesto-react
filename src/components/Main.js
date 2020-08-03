5985c4d6c77a5f27ee0b32e6071566bf536631cf
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-08-03 19:33:52 +0300

    отредактировал Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,65 +2,71 @@ import React from 'react';
 import api from '../utils/api';
 import Card from './Сard';
 import pen from '../images/button/pen.svg';
+import {CurrentUserContext} from '../contexts/CurrentUserContext';
 
 export default function Main({onEditProfile, onAddPlace, onEditAvatar, handleCardClick}){
 
-    const [user, getUserData] = React.useState({
-        userName : '', 
-        userDescription: '', 
-        userAvatar: ''
-    });
+    const currentUser = React.useContext(CurrentUserContext);
 
-    const [cards, getCards] = React.useState([]);
+    const [cards, setCards] = React.useState([]);
+
+    function handleCardLike(card) {
+        const isLiked = card.likes.some(i => i._id === currentUser._id);
+        api.addLikeOrDislikeCard(card._id, !isLiked).then((newCard) => {
+          const newCards = cards.map((c) => c._id === card._id ? newCard : c);
+          setCards(newCards);
+        });
+    }
+
+    function handleCardDelete(card){
+        api.deleteCard(card._id).then((newCard) => {
+            console.log('newCard', newCard);
+            //const newCards = cards.filter((c) => c._id === card._id ? newCard : c);
+            //setCards(newCards);
+          });
+    }
 
     function showCards(){
         api.getCards() 
             .then((res) => {
-                getCards(res);
+                setCards(res);
             })
             .catch((error) => console.log('Ошибка при первичной загрузке карточек', error));
     }
 
-    function showUserInfo(){
-        api.getUserInfo()
-            .then((res) => {
-                const {avatar, name, about, _id} = res;
-                getUserData({
-                    userName : name, 
-                    userDescription: about, 
-                    userAvatar: avatar
-                });
-            })
-            .catch((error) => console.log('Ошибка при первичной загрузке данных пользователя', error));
-    }
-
     React.useEffect(() => {
         showCards();
-        showUserInfo();
     },[]);
 
-    const {userName, userAvatar, userDescription} = user;
+    const {name, avatar, about} = currentUser;
 
     return(
         <main className="main">
             <section className="profile">
             <div className="profile__container-avatar">
                 <div className="profile__avatar-opacity" src={`${pen}`} onClick={onEditAvatar}/>
-                <img alt="Аватарка пользователя" className="profile__avatar" src={`${userAvatar}`}/>
+                <img alt="Аватарка пользователя" className="profile__avatar" src={`${avatar}`}/>
             </div>
             <div className="profile-info">
                 <div className="profile-info__container">
-                    <h1 className="profile-info__name">{userName}</h1>
+                    <h1 className="profile-info__name">{name}</h1>
                     <button className="profile-info__edit-button" type="button" onClick={onEditProfile}/>
                 </div>
-                <p className="profile-info__information-person">{userDescription}</p>
+                <p className="profile-info__information-person">{about}</p>
             </div>
             <button className="profile__add-button" type="button" onClick={onAddPlace}/>
             </section>
             <section className="elements">
                 {
                     cards && 
-                    cards.map(({_id, ...props}) => <Card key={_id} {...props} onCardClick={(linkCard) => handleCardClick(linkCard)}/>)
+                    cards.map((card) => 
+                        <Card 
+                            key={card._id} 
+                            {...card}
+                            onCardClick={(linkCard) => handleCardClick(linkCard)}
+                            onCardLike={(card) => handleCardLike(card)}
+                            onCardDelete={(card) => handleCardDelete(card)}
+                        />)
                 }
             </section>
       </main>
