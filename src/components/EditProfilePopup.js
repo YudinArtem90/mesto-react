dfbd3ac55c605b65335f4c537bddba5f524cd7ed
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-08-03 19:34:46 +0300

    Добавил EditProfilePopup + CurrentUserContext

---
+++ src/components/EditProfilePopup.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PopupWithForm from './PopupWithForm.js';
+import {CurrentUserContext} from '../contexts/CurrentUserContext';
+
+function EditProfilePopup(props){
+    const {isOpen, onClose} = props;
+    const [name, setName] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const currentUser = React.useContext(CurrentUserContext);
+
+    React.useEffect(() => {
+        setName(currentUser.name);
+        setDescription(currentUser.about);
+      }, [currentUser]);
+
+    function onChangeName(e){
+        setName(e.target.value);
+    }
+
+    function PopupElementEditProfile(){
+        return (
+            <>
+                <input 
+                    id="name_person" 
+                    className="popup__field popup__field_name_person" 
+                    type="text" 
+                    name="namePerson" 
+                    minLength={2} 
+                    maxLength={40} 
+                    onChange={onChangeName}
+                    value={name}
+                    pattern="^[A-Za-zА-Яа-яЁё\s\-]+$" 
+                    required 
+                />
+                <span id="name_person-error" className="popup__field-error" />
+                <input 
+                    id="inform_person" 
+                    className="popup__field popup__field_inform_person" 
+                    type="text" 
+                    name="informPerson" 
+                    minLength={2} 
+                    maxLength={200}  
+                    value={description} 
+                    required 
+                />
+                <span id="inform_person-error" className="popup__field-error" />
+            </>
+        );
+    }
+    
+    return(
+        <PopupWithForm
+            children={<PopupElementEditProfile/>}
+            title='Редактировать профиль'
+            name ='popupEditProfile'
+            isOpen={isOpen}
+            onClose={onClose}
+            textButton='Сохранить'
+        />
+    );
+}
+
+export default EditProfilePopup;
